827c67e4a041cff2921521b62da3997229ff92c0
Reiku <reiku30@live.fr>
2014-01-20 23:30:28 +0100

    Changement IP

--- src/client/App.java
+++ src/client/App.java
@@ -8,7 +8,7 @@ public class App {
 	public static ServerListener socket;
 	
 	public static void main(String[] args){
-		socket = new ServerListener("81.251.232.69", 3829);
+		socket = new ServerListener("127.0.0.1", 3829);
 		LoginController loginController = new LoginController(socket);
 		Login login = new Login(loginController);
 		socket.setObserver(login);
