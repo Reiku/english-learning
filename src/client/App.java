cc6b1a32721c36198c2a5112dfa90e4928e4459f
Reiku <reiku30@live.fr>
2014-01-28 22:23:33 +0100

    Update App.java

--- src/client/App.java
+++ src/client/App.java
@@ -1,9 +1,13 @@
 package client;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import client.controller.LoginController;
 import client.listener.ServerListener;
 import client.view.Login;
-
 import common.User;
 
 public class App {
@@ -11,8 +15,17 @@ public class App {
 	public static User user;
 	
 	public static void main(String[] args){	
-		socket = new ServerListener("62.210.243.12", 3829);
-		Login login = new Login(new LoginController(socket));
-		socket.setObserver(login);
+		Properties properties = new Properties();
+		try {
+			File config = new File("config.txt");
+			FileInputStream input = new FileInputStream(config);
+			properties.load(input);
+			input.close();
+			socket = new ServerListener(properties.getProperty("serverip"), Integer.valueOf(properties.getProperty("serverport")));
+			Login login = new Login(new LoginController(socket));
+			socket.setObserver(login);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
